83fa430397e1c354ee3d793d8efab48f259fc2a4
Ye Shaw <enzuovip@qq.com>
2017-04-27 23:07:35 +0800

    independence module
     var store = new Store();
     store.on("ready", function() {
         store.on("date:enteringState", function(state) {
             console.log("data:enteringState" + state)
         });
    
         store.on("date:leavingState", function(state) {
             console.log("date:leavingState" + state);
         });
    
          store.on("test:enteringState", function(state) {
             console.log("data:enteringState" + state)
         });
    
         store.on("test:leavingState", function(state) {
             console.log("date:leavingState" + state);
         });
    
         store.init("date", 000000)
    
         store.init("test",111111111)
    
         store.test = 33333333
    
         store.date = 44444444;
    
     })
    
     store.emit("ready");

--- javascript/lib/Store.js
+++ javascript/lib/Store.js
@@ -1,255 +1,86 @@
-function EventDispatcher() {
-    this.listeners = {};
-}
+ var Store = (function() {
 
-EventDispatcher.prototype = {
-    on: function(type, listener) {
-        if (!this.listeners[type]) {
-            this.listeners[type] = [];
-        }
-        this.listeners[type].push(listener);
-    },
-    emit: function(type) {
-        var listeners = this.listeners[type];
-        var args = Array.prototype.slice.call(arguments);
-        args.shift();
-        if (typeof window.console !== "undefined") {
-            window.console.info("on\t" + type + "\t" + args.join(" , "));
-        }
-        if (listeners) {
-            for (var i = 0, len = listeners.length; i < len; i++) {
-                listeners[i].apply(this, args);
-            }
-        }
-    },
-    off: function(type) {
-        delete this.listeners[type];
-    }
-};
+     var EventDispatch = (function() {
+         var EventDispatch = function() {
+             this._listener = {}
+         }
 
-function _predefine(obj, key, value) {
-    Object.defineProperty(obj, key, {
-        value: value,
-        enumerable: false,
-        writable: false,
-        configurable: false
-    });
-}
+         EventDispatch.prototype = {
+             constructor: EventDispatch,
+             on: function(type, fn) {
+                 if (!this._listener[type]) {
+                     this._listener[type] = [];
+                 }
+                 var args = Array.prototype.slice.call(arguments, 1);
+                 for (var i = 0; i < args.length; i++) {
+                     this._listener[type].push(args[i]);
+                 }
+                 return this;
+             },
+             emit: function(type) {
+                 var arr = Array.prototype.slice.call(arguments);
+                 arr.shift();
+                 if (this._listener[type]) {
+                     for (var i = 0; i < this._listener[type].length; i++) {
+                         this._listener[type][i].apply(this, arr);
+                     }
+                 }
+                 return this;
+             },
+             remove: function(type) {
+                 if (this._listener[type]) {
+                     delete this._listener[type];
+                 }
+                 return this;
+             }
+         }
+         return EventDispatch;
+     })();
 
-var DEFAULT_OPT = Object.freeze({
-    readonly: false,
-    // READONLY: cannot delete or directly modify
-    // but you can set it's descendants value if it's an object
-    state: false,
-    // STATE: upon changing, trigger 2 events
-    // "enteringState" / "leavingState"
-    // instead of a mere "changed" event
-    noerror: true,
-});
+     var Store = function() {
+         if (!(this instanceof Store)) {
+             throw new TypeError("A store instance should be new");
+             return;
+         }
+         this._data = {};
+     }
 
-function Store() {
-    if (!(this instanceof Store)) {
-        throw new TypeError("A store instance should be new");
-        return;
-    }
+     Store.prototype = {
+         constructor: Store,
+         define: function(type) {
+             var defines = {
+                 enumerable: true,
+                 configurable: true,
+                 get: function() {
+                     return this._data[type];
+                 },
+                 set: function(newValue) {
+                     var preValue = this._data[type];
+                     if (newValue === preValue) {
+                         this.emit(type + ":enteringState", newValue);
+                     } else {
+                         this.emit(type + ":leavingState", preValue);
+                         this._data[type] = newValue;
+                         this.emit(type + ":enteringState", newValue);
+                         this.emit(type + ":changed", newValue);
+                     }
+                 }
+             }
+             Object.defineProperty(this, type, defines);
+         },
+         event: new EventDispatch(),
+         init: function(type, value) {
+             this.define(type);
+             this._data[type] = value;
+             this[type] = this._data[type];
+         },
+         on: function(type, _listener) {
+             this.event.on.call(this.event, type, _listener);
+         },
+         emit: function(type) {
+             this.event.emit.apply(this.event, arguments);
+         }
+     }
 
-    var __data = {},
-        __opt = {},
-        __fn = {},
-        store = this;
-    _predefine(this, "event", new EventDispatcher());
-
-    function define(type, option) {
-        var defines = {
-            enumerable: true,
-            get: function() {
-                return __data[type];
-            }
-        }
-
-        __opt[type] = option;
-
-        if (option.readonly) {
-            Object.defineProperty(store, type, defines);
-            return;
-        }
-
-        defines.configurable = true;
-        if (typeof option.set === "function") {
-            defines.set = option.set;
-        } else if (!option.state) {
-            defines.set = function(newv) {
-                if (newv === __data[type]) {
-                    return;
-                }
-                __data[type] = newv;
-                store.emit(type + ":changed", newv);
-            }
-        } else {
-            defines.set = function(newv) {
-                var prev = __data[type]
-                if (newv === prev) {
-                    return;
-                }
-
-                store.emit(type + ":leavingState", prev);
-                __data[type] = newv;
-                store.emit(type + ":enteringState", newv);
-            }
-        }
-
-        Object.defineProperty(store, type, defines);
-    }
-
-    function getVal(initer, noerror) {
-        try {
-            return typeof initer === "function" ? initer() : initer;
-        } catch (err) {
-            if (noerror) {
-                return;
-            }
-            throw err;
-        }
-    }
-
-    _predefine(this, "init", function(type, initer, opt) {
-        var option = _.defaults(opt || {}, DEFAULT_OPT);
-        option.initializer = typeof initer === "function" ? "function call" : "value assign";
-
-        var val = getVal(initer, option.noerror);
-        if (val === null || typeof val === "undefined") {
-            return false;
-        }
-
-        if (type in __opt) {
-            // if defined， use outer initial
-            this[type] = val;
-            return true;
-        }
-
-        define(type, option);
-        __data[type] = val; // inner initial
-
-        if (!option.state) {
-            store.emit(type + ":ready", __data[type]);
-        } else {
-            store.emit(type + ":enteringState", __data[type]);
-        }
-
-        return true;
-    });
-
-    _predefine(this, "valueOf", function(type) {
-        return "[Store]";
-    });
-    _predefine(this, "getDescriptorOf", function(type) {
-        if (!(type in __opt)) {
-            return null;
-        }
-        var opt = JSON.parse(JSON.stringify(__opt[type]));
-        return _.extend(opt, {
-            value: __data[type],
-        });
-    });
-    _predefine(this, "eventsHappened", {});
-}
-
-Store.prototype = {
-    upon: function(evt, fn) {
-        this.uponAll([evt], fn);
-    },
-    uponAll: function(args, fn) {
-        if (!(args instanceof Array) || typeof fn !== "function") {
-            throw new TypeError("Parameter error, should be array and function");
-        }
-
-        var counter = (function(total, callback) {
-            var i = 0;
-            return function() {
-                i++;
-                if (i >= total) {
-                    callback();
-                }
-            }
-        })(args.length, fn);
-
-        var store = this;
-        args.forEach(function(evtName) {
-            if (!(evtName in store.eventsHappened)) {
-                store.once(evtName, counter);
-            } else {
-                counter();
-            }
-        });
-    },
-    uponAny: function(args, fn) {
-        if (!(args instanceof Array) || typeof fn !== "function") {
-            throw new TypeError("Parameter error, should be array and function");
-        }
-
-        var counter = (function(total, callback) {
-            var i = 0;
-            return function() {
-                i++;
-                if (i === 1) {
-                    callback();
-                }
-            }
-        })(args.length, fn);
-
-        var store = this;
-        args.forEach(function(evtName) {
-            if (!(evtName in store.eventsHappened)) {
-                store.once(evtName, counter);
-            } else {
-                counter();
-            }
-        });
-    },
-    uponNone: function(args, fn) {
-        if (!(args instanceof Array) || typeof fn !== "function") {
-            throw new TypeError("Parameter error, should be array and function");
-        }
-
-        var store = this;
-        args.every(function(evtName) {
-            return !(evtName in store.eventsHappened);
-        }) && fn();
-    },
-    print: function(event){
-        console.log(event);
-    },
-};
-
-[
-    "addListener", "on", "once",
-    "removeListener", "removeAllListeners",
-    "setMaxListeners", "listeners", "listenerCount"
-].forEach(function(key) {
-    Store.prototype[key] = function() {
-        this.event[key].apply(this.event, arguments);
-    };
-});
-Store.prototype.emit = function() {
-    this.eventsHappened[arguments[0]] = 1;
-    this.print(arguments[0]);
-    this.event.emit.apply(this.event, arguments);
-};
-Store.prototype.constructor = Store;
-
-Object.defineProperty(Store.prototype, "allListener", {
-    get: function() {
-        return Object.keys(this.event._events);;
-    },
-    enumerable: false,
-    configurable: false
-});
-Object.defineProperty(Store.prototype, "allData", {
-    get: function() {
-        return Object.keys(this);
-    },
-    enumerable: false,
-    configurable: false
-})
-
-module.exports = Store;
+     return Store;
+ })();
